3358bb637bfc3db6ee83666473198da392bba3b3
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-11-08 09:36:46 +0200

    last commit from 07.11

--- Practice_0711/linkedlist/reverse.js
+++ Practice_0711/linkedlist/reverse.js
@@ -1,140 +1,139 @@
-class Node{
-    constructor(val){
-        this.val = val;
-        this.next = null;
-    }
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
-class SinglyLinkedList{
-    constructor(){
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    push(val){
-        let newNode = new Node(val);
-        if(!this.head){
-            this.head = newNode;
-            this.tail = this.head;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.length++;
-        return this;
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
     }
-    pop(){
-        if(!this.head) return undefined;
-        let current = this.head;
-        let newTail = current;
-        while(current.next){
-            newTail = current;
-            current = current.next;
-        }
-        this.tail = newTail;
-        this.tail.next = null;
-        this.length--;
-        if(this.length === 0){
-            this.head = null;
-            this.tail = null;
-        }
-        return current;
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
     }
-    shift(){
-        if(!this.head) return undefined;
-        let currentHead = this.head;
-        this.head = currentHead.next;
-        this.length--;
-        if(this.length === 0){
-            this.tail = null;
-        }
-        return currentHead;
+    return current;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
     }
-    unshift(val){
-        let newNode = new Node(val);
-        if(!this.head) {
-            this.head = newNode;
-            this.tail = this.head;
-        }
-        newNode.next = this.head;
-        this.head = newNode;
-        this.length++;
-        return this;
+    return currentHead;
+  }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
     }
-    get(index){
-        if(index < 0 || index >= this.length) return null;
-        let counter = 0;
-        let current = this.head;
-        while(counter !== index){
-            current = current.next;
-            counter++;
-        }
-        return current;
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
     }
-    set(index, val){
-        let foundNode = this.get(index);
-        if(foundNode){
-            foundNode.val = val;
-            return true;
-        }
-        return false;
+    return current;
+  }
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
     }
-    insert(index, val){
-        if(index < 0 || index > this.length) return false;
-        if(index === this.length) return !!this.push(val);
-        if(index === 0) return !!this.unshift(val);
+    return false;
+  }
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
 
-        let newNode = new Node(val);
-        let prev = this.get(index - 1);
-        let temp = prev.next;
-        prev.next = newNode;
-        newNode.next = temp;
-        this.length++;
-        return true;
+    let newNode = new Node(val);
+    let prev = this.get(index - 1);
+    let temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    let previousNode = this.get(index - 1);
+    let removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
     }
-    remove(index){
-        if(index < 0 || index >= this.length) return undefined;
-        if(index === 0) return this.shift();
-        if(index === this.length - 1) return this.pop();
-        let previousNode = this.get(index - 1);
-        let removed = previousNode.next;
-        previousNode.next = removed.next;
-        this.length--;
-        return removed;
+    return this;
+  }
+  print() {
+    let arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
     }
-    reverse(){
-      let node = this.head;
-      this.head = this.tail;
-      this.tail = node;
-      let next;
-      let prev = null;
-      for(let i = 0; i < this.length; i++){
-        next = node.next;
-        node.next = prev;
-        prev = node;
-        node = next;
-      }
-      return this;
-    }
-    print(){
-        let arr = [];
-        let current = this.head
-        while(current){
-            arr.push(current.val)
-            current = current.next
-        }
-        console.log(arr);
-    }
+    console.log(arr);
+  }
 }
 
-let list = new SinglyLinkedList()
+let list = new SinglyLinkedList();
 
-list.push(100)
-list.push(201)
-list.push(250)
-list.push(350)
-list.push(999)
-
+list.push(100);
+list.push(201);
+list.push(250);
+list.push(350);
+list.push(999);
 
 //list.print() to see all the elements in console
 // list.reverse() should gives us the reverse list
--- Practice_0711/mergeSort/mergeSort.js
+++ Practice_0711/mergeSort/mergeSort.js
@@ -1,35 +1,41 @@
 // Merge function from earlier
-function merge(arr1, arr2){
-    let results = [];
-    let i = 0;
-    let j = 0;
-    while(i < arr1.length && j < arr2.length){
-        if(arr2[j] > arr1[i]){
-            results.push(arr1[i]);
-            i++;
-        } else {
-            results.push(arr2[j])
-            j++;
-        }
+function merge(arr1, arr2) {
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      console.log(results);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      console.log(results);
+      j++;
     }
-    while(i < arr1.length) {
-        results.push(arr1[i])
-        i++;
-    }
-    while(j < arr2.length) {
-        results.push(arr2[j])
-        j++;
-    }
-    return results;
+  }
+  console.log("OUT OF LOOP 1");
+  console.log(results);
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    console.log(results);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    console.log(results);
+    j++;
+  }
+  return results;
 }
 
 // Recrusive Merge Sort
-function mergeSort(arr){
-    if(arr.length <= 1) return arr;
-    let mid = Math.floor(arr.length/2);
-    let left = mergeSort(arr.slice(0,mid));
-    let right = mergeSort(arr.slice(mid));
-    return merge(left, right);
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr; // if there's less than two elements, array is sorted
+  let mid = Math.floor(arr.length / 2); // find midpoint and split
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
 }
 
-console.log(mergeSort([10,24,76,73]))
+console.log(mergeSort([10, 25, 7, 77, 24, 2, 76, 73, 1, 35, 42]));
