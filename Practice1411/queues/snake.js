75ecf5371b06ba45fe500d24f26ce63a4e7f458c
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-11-14 13:17:26 +0200

    add valid snake

--- Practice1411/queues/snake.js
+++ Practice1411/queues/snake.js
@@ -1,3 +1,8 @@
+// snake
+// - queue for snake body (array)
+// - draw the grid with the snakes body
+// - move - take in a direction, manipulate queues
+// - play(), not related to DS
 class Snake {
   constructor() {
     this.snakeBody = [
@@ -7,7 +12,6 @@ class Snake {
       [4, 4],
     ];
   }
-
   move(direction) {
     const delta = {
       up: [-1, 0],
@@ -22,13 +26,12 @@ class Snake {
     this.snakeBody.push(newHead);
     this.snakeBody.shift();
   }
-
   draw() {
     const grid = [];
     for (let i = 0; i < 10; i++) {
       const row = [];
       for (let j = 0; j < 10; j++) {
-        row.push("");
+        row.push(" ");
       }
       grid.push(row);
     }
@@ -38,5 +41,24 @@ class Snake {
     });
     console.clear();
     grid.forEach((row) => console.log(row.join("|")));
+    //console.log(grid)
+  }
+  // not related to DS just for fun :)
+  play() {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.resume();
+    stdin.setEncoding("utf8");
+    stdin.on("data", (keypress) => {
+      if (keypress === "w") this.move("up");
+      if (keypress === "a") this.move("left");
+      if (keypress === "s") this.move("down");
+      if (keypress === "d") this.move("right");
+      if (keypress === "y") process.exit();
+      this.draw();
+    });
   }
 }
+const game = new Snake();
+console.log(game);
+game.play();
